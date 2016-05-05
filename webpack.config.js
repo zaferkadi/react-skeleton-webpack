e160f8a5735e7fe0b29996e867136f90b4f8153e
Zafer <zafer.qadi@gmail.com>
2016-05-05 03:50:40 +0300

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,32 @@
+'use strict';
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
+
 module.exports = {
-	entry: './main.js',
-	output:{
-		path:'./',
-		filename: 'index.js'
+	entry: './js/main.jsx',
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		publicPath: "/dist/",
+		filename: 'bundle.js'
 	},
 	devServer: {
-		inline:true,
-		port:3333
+		inline: true,
+		port: 3333
 	},
 	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader : 'babel',
-				query: {
-					presets: ['es2015', 'react']
-				}
+		loaders: [{
+			test: /\.jsx?$/,
+			exclude: /node_modules/,
+			loader: 'babel',
+			query: {
+				presets: ['es2015', 'react']
 			}
-		]
-	}
-}
+		}, {
+			test: /\.scss$/,
+			loader: ExtractTextPlugin.extract("style","css!sass")
+				 
+		}]
+	},
+	plugins: [new ExtractTextPlugin("style.css")]
+};
